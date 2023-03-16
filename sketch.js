01fcfffdfaa8c2e961f1dd35404921b90be94ba4
BabyRamen <ysgerkri@gmail.com>
2023-03-16 15:24:12 +0100

    16-03-2023
    Fede lektier

--- sketch.js
+++ sketch.js
@@ -3,24 +3,27 @@ let puzzles
 let images = []
 let nice = true
 
-function preload(){
-  for (i=0; i<15; i++) {
-    let image = loadImage("assets/img_"+i+".png");
-images.push(image)
+function preload() {
+  for (i = 0; i < 15; i++) {
+    for (y = 0; y < 15; y++) {
+      let image = loadImage("assets/img" + i + ".png");
+      images.push(image)
+      tiles.push(new tile(400 * i, 400 * y, images[i], 400, 400))
+    }
   }
+  print(tiles)
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1600, 1600);
 }
 
 function draw() {
   background(220);
 
-  for (let tile of tiles){
+  for (let tile of tiles) {
+    
     tile.display();
   }
-if (nice=true){
-  tiles.push(new tile(width/4,length/4,random(images)))
-}
+
 }
--- tile.js
+++ tile.js
@@ -1,14 +1,16 @@
 class tile {
-    constructor(x,y,img){
+    constructor(x,y,img,width,height){
         this.x=x
         this.y=y
         this.img=img
+        this.width=width
+        this.height=height
     }
     display(){
         stroke(255)
         strokeWeight(4)
         noFill()
-        image=(this.img, this.x, this.y)
+        image(this.img, this.x, this.y,this.width,this.height)
     }
     
 }
