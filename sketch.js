d2e01c14fcbf8967d2c04ddc5c33d20028062fe7
BabyRamen <ysgerkri@gmail.com>
2023-03-22 09:23:38 +0100

    Yaaaay

--- sketch.js
+++ sketch.js
@@ -1,29 +1,59 @@
-const tiles = []
+let tiles = []
 let puzzles
 let images = []
 let nice = true
 
 function preload() {
+
+  for (i = 0; i < 16; i++) {
+    let image = loadImage("assets/img" + i + ".png")
+    tiles.push(new tile(image, 400, 400))
+  }
+
+  /*
+
   for (i = 0; i < 4; i++) {
     for (y = 0; y < 4; y++) {
       let image = loadImage("assets/img" + i*i + ".png");
       images.push(image)
-      tiles.push(new tile(400 * i, 400 * y, images[1], 400, 400))
+      console.log(images[1])
+      tiles.push(new tile(400 * i, 400 * y, image, 400, 400))
     }
-  }
+  }*/
   print(tiles)
+  tiles.splice(15)
 }
 
 function setup() {
   createCanvas(1600, 1600);
+  tiles = shuffle(tiles)
 }
 
 function draw() {
   background(220);
 
-  for (let tile of tiles) {
-    
-    tile.display(); 
+  let x = 0
+  let y = 0
+
+  for (let i = 0; i < tiles.length; i++) {
+    stroke(255)
+    strokeWeight(4)
+    noFill()
+    console.log("tile")
+    console.log(x)
+    console.log(y)
+    let tile = tiles[i]
+    image(tile.img, x, y, tile.width, tile.height)
+    if (x >= width - width/4) {
+      y += height / 4
+    }
+    if (x < width - width/4) {
+      x += width / 4
+    } else {
+      x = 0
+    }
   }
+  noLoop()
 
 }
+
--- tile.js
+++ tile.js
@@ -1,16 +1,18 @@
 class tile {
-    constructor(x, y, img, width, height) {
-        this.x = x
-        this.y = y
+    constructor(img, width, height) {
         this.img = img
-       this.width = width
+        this.width = width
         this.height = height
     }
     display() {
         stroke(255)
         strokeWeight(4)
         noFill()
-        image(this.img, this.x, this.y, this.width, this.height)
+        //image(this.img, this.x, this.y, this.width, this.height)
+        //image(this.img, this.x, this.y)
+        
+        //rect(this.x, this.y, this.width, this.height)
+        //image(this.img, this.x, this.y, this.width, this.height)
     }
 
 }
